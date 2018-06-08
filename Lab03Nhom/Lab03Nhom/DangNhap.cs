2e1d962e553fe85aab9448049ec1c3db5458bbe8
hoanglan <arsenalclub96@gmail.com>
2018-06-08 18:33:46 +0700

    up code

--- Lab03Nhom/Lab03Nhom/DangNhap.cs
+++ Lab03Nhom/Lab03Nhom/DangNhap.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,48 @@ namespace Lab03Nhom
 {
     public partial class DangNhap : Form
     {
+        public static string name;
+        public static string password;
         public DangNhap()
         {
             InitializeComponent();
         }
+        
+        string connectstring = "Data Source=HOANGLAN;Initial Catalog=QLSVN;Integrated Security=True";
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectstring);
+            name = txtname.Text;
+            password = txtpass.Text;
+            string query = "select * from NHANVIEN where MANV = '"+name+"' and hashbytes('sha1','"+password+"') = MATKHAU";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            connection.Close();
+
+            int check = ds.Tables[0].Rows.Count;
+            if (check > 0)
+            {
+                
+                if(DialogResult.OK == MessageBox.Show("Đăng nhập thành công!!!"))
+                {
+                    DanhSachLop dslop = new DanhSachLop();
+                    this.Hide();
+                    dslop.ShowDialog();
+                    //this.Close();
+                }
+                
+            }
+            else
+                MessageBox.Show("Đăng nhập thất bại!!! Sai tên đăng nhập hoặc mật khẩu");
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
     }
 }
--- Lab03Nhom/Lab03Nhom/DanhSachLop.cs
+++ Lab03Nhom/Lab03Nhom/DanhSachLop.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Lab03Nhom
 {
@@ -15,6 +17,53 @@ namespace Lab03Nhom
         public DanhSachLop()
         {
             InitializeComponent();
+            
+        }
+        
+        string connectstring = "Data Source=HOANGLAN;Initial Catalog=QLSVN;Integrated Security=True";
+
+        private void DanhSachLop_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectstring);
+           
+            string name = DangNhap.name;
+            string pass = DangNhap.password;
+        
+            string query = "select * from NHANVIEN where MANV = '" + name + "' and hashbytes('sha1','" + pass + "') = MATKHAU";
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            SqlDataReader r = command.ExecuteReader();
+            if (r.Read())
+            {
+                txtmanv.Text = r.GetValue(0).ToString();
+                txthoten.Text = r.GetValue(1).ToString();
+                txtemail.Text = r.GetValue(2).ToString();
+            }
+            connection.Close();
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; 
+            dataGridView1.DataSource = GetLopHoc().Tables[0];
+        }
+
+        DataSet GetLopHoc()
+        {
+            string manv = DangNhap.name;
+            DataSet data = new DataSet();
+            string query = "select l.MALOP, l.TENLOP from NHANVIEN nv, LOP l where l.MANV = nv.MANV and nv.MANV = '"+manv+"'";
+            SqlConnection conn = new SqlConnection(connectstring);
+            conn.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            adapter.Fill(data);
+            conn.Close();
+            return data;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThemLop lop = new ThemLop();
+            this.Hide();
+            lop.Show();
+
         }
     }
 }
