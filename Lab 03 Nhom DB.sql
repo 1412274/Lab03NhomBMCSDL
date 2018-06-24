6c5ba78b213303e78370d8a1d3a5a8c6dd454eb0
1412274 <nhkim141996@gmail.com>
2018-06-24 23:27:07 +0700

    Them file script

---
+++ Lab 03 Nhom DB.sql
@@ -0,0 +1,18 @@
+﻿/*----------------------------------------------------------
+MASV: 1412274, 1412282, 1412414
+HO TEN CAC THANH VIEN NHOM:
+	1412274 - Nguyen Hoang Kim
+	1412282 - Nguyen Hoang Lan
+	1412414 - Vuong Thien Phu
+LAB: 03 - NHOM
+NGAY: 12/06/2018
+----------------------------------------------------------*/
+--CAU LENH TAO DB
+CREATE DATABASE QLSVN
+GO
+
+USE QLSVN
+GO
+
+ALTER DATABASE QLSVN
+SET COMPATIBILITY_LEVEL = 120
